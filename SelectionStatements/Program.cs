a807fefd4012112975479a57700ce789cfe14a9e
Mona Lisa <email@example.com>
2020-08-25 10:26:11 -0600

    testing

--- SelectionStatements/Program.cs
+++ SelectionStatements/Program.cs
@@ -6,7 +6,26 @@ namespace SelectionStatements
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var r = new Random();
+            var favNumber = r.Next(1, 1000); // gens a random number 
+
+            Console.WriteLine("Guess what number I'm thinking?");
+
+            var userInput = int.Parse(Console.ReadLine());
+
+            if(userInput < favNumber)
+            {
+                Console.WriteLine($"You guessed too low.");
+            }
+            else if(userInput > favNumber)
+            {
+                Console.WriteLine($"You guessed too high.");
+            }
+            else
+            {
+                Console.WriteLine("You got me!!!");
+            }
+
         }
     }
 }
